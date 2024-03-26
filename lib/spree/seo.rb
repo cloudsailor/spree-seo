fc6994fb4ac57b3740ff550a2cf2ef668c747a1b
Jibran Usman <jibran.usman@hotmail.com>
2024-03-26 06:25:21 -0300

    further cleanup

--- lib/spree/seo.rb
+++ lib/spree/seo.rb
@@ -9,21 +9,6 @@ require 'rails/engine/commands'
 require 'spree_core'
 require 'spree_api'
 
-require 'sprockets/railtie'
-
-require 'jquery-rails'
-require 'jquery-ui-rails'
-require 'select2-rails'
-require 'flatpickr'
-require 'flag-icons-rails'
-require 'bootstrap'
-require 'glyphicons'
-require 'hotwire-rails'
-require 'inline_svg'
-require 'jsbundling-rails'
-require 'responders'
-require 'tinymce-rails'
-
 require_relative "seo/version"
 
 module Spree
