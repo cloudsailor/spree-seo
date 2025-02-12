768d597f26452a04adbfa9aed3d460b070736f57
Tomasz Strzeszewski <tomasz.strzeszewski@toptal.com>
2025-02-12 16:17:56 +0100

    Remove ENGINE_ROOT to fix migrations problem

--- lib/spree/seo.rb
+++ lib/spree/seo.rb
@@ -1,11 +1,9 @@
 require "spree/seo/version"
 require "spree/seo/engine"
 
-ENGINE_ROOT = File.expand_path('../..', __FILE__)
 ENGINE_PATH = File.expand_path('../../lib/spree/seo/engine', __FILE__)
 
 require 'rails/all'
-require 'rails/engine/commands'
 require 'spree_core'
 require 'spree_api'
 
